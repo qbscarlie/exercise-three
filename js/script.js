b8c2a9be0c2d5ec7df9c2cd58535396dd5ff9545
qbscarlie <qbscarlie@gmail.com>
2016-02-16 19:46:36 -0600

    First commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 $(document).ready(function() {
-    console.log("Hello world.")
+    getData();
 });
 
+function getData() {
+	$.getJSON("js/columbia.json", function(schoolsData) {
+		//Callback function runs after the data is loaded.
+		loopThroughData(schoolsData);
 
+	})
+}
 
+function loopThroughData(s) {
+	for(i=0;i<s.length;i++)
+	{
+		var schoolName = s[i].SCHOOL_NAME;
+		var year = s[i].YEAR;
+		var gradPercent = s[i].GRADUATES_PCT;
 
+		//if(gradPercent < 60 && gradPercent != 0)
+		//	console.log(schoolName, gradPercent);
 
+		var barWidth = gradPercent * 10;
+
+		if(schoolName==="FREDERICK DOUGLASS HIGH"){
+			$(".chart").append("<div class='bar' style='width: "+barWidth+"px'></div>");
+		}
+	}
+
+}
 
